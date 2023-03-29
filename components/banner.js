2d830c2f4f8d0063aefaf92fd131c923f527334e
Balaganesh003 <k.balaganesh26@gmail.com>
2023-03-29 09:57:48 +0530

    updated the name

--- components/banner.js
+++ components/banner.js
@@ -1,11 +1,11 @@
-import styles from "./banner.module.css";
+import styles from './banner.module.css';
 
 const Banner = (props) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Coffee</span>
-        <span className={styles.title2}>Connoisseur</span>
+        <span className={styles.title1}>Latte</span>
+        <span className={styles.title2}>Locator</span>
       </h1>
       <p className={styles.subTitle}>Discover your local coffee stores!</p>
       <div className={styles.buttonWrapper}>
